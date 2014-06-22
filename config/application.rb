9a3f14d7f51b0c1ecfbe0ec34b5a84c8bb373387
Chris Patuzzo <chris@patuzzo.co.uk>
2014-06-22 11:00:47 +0100

    Fix fonts for Heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ Bundler.require(*Rails.groups)
 
 module Ashdownlandscapes
   class Application < Rails::Application
+    config.assets.precompile << /\.(?:eot|woff|ttf|svg)$/
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
