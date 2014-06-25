6f8723fdd7f9d53f37f8ff2f4bc00dc0d68a3221
Chris Patuzzo <chris@patuzzo.co.uk>
2014-06-25 21:54:30 +0100

    More progress on the Photos page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,18 @@ class PagesController < ApplicationController
   end
 
   def photos
+    @current_page = page
+    @photos = ChunkedPhotos.for_page(@current_page)
+    @total_pages = ChunkedPhotos.total_pages
   end
 
   def contact
+  end
+
+  private
+  def page
+    page = params[:page].to_i
+    page.zero? ? 1 : page
   end
 
 end
---
+++ app/services/chunked_photos.rb
@@ -0,0 +1,17 @@
+module ChunkedPhotos
+  PHOTOS_PER_PAGE = 8
+  PHOTOS_PER_ROW  = 4
+  PHOTO_COUNT     = 25
+
+  def self.for_page(page)
+    first = (page - 1) * PHOTOS_PER_PAGE + 1
+    last = first + PHOTOS_PER_PAGE - 1
+    last = last > PHOTO_COUNT ? PHOTO_COUNT : last
+
+    (first..last).each_slice(PHOTOS_PER_ROW)
+  end
+
+  def self.total_pages
+    (PHOTO_COUNT.to_f / PHOTOS_PER_PAGE).ceil
+  end
+end
